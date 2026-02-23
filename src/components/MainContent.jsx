24dd8815846152001507bc817fee5c4e86eecb04
4bush <abusahmanmansur2@gmail.com>
2026-02-23 13:42:09 +0500

    Add files via upload

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MainContent() {
+  return (
+    <main className="main">
+      <h2>Welcome!</h2>
+      <p>This is my React SPA project.</p>
+    </main>
+  );
+}
+
+export default MainContent;
